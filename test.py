6272268b3fc360eea389d2a00f482901608ac1fa
Wang Zhechao <475031247@qq.com>
2024-05-15 16:27:26 +0800

    modify doc

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("hello word")
+str_1 = "abcdefghijklmnop"
+str_2 = "abcsafjklmnopqrstuvw"
+if len(str_1) > len(str_2):
+    str_1, str_2 = str_2, str_1
+
+for l in range(len(str_1), 1, -1):
+    for i in range(0, len(str_1)-l):
+        print(str_1[i:i+l])
